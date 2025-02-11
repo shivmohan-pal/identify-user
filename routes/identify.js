c7dcada1825d48f1e24a1e48f91a98634a27451d
shivmohan-pal <64728396+shivmohan-pal@users.noreply.github.com>
2025-02-11 20:34:05 +0530

    Revert "null check and checking for one entry number or email"
    This reverts commit d2d4de6afe33fd57bacc10429c2b7e5e9bcbee32.

--- routes/identify.js
+++ routes/identify.js
@@ -1,52 +1,7 @@
 const identify = require("express").Router();
 
-identify.post("/", async (req, res) => {
-  const { email, phoneNumber } = req.body;
+identify.post("/", (req, res) => {
 
-  // if Both email and phoneNumber are missing
-  if (!email && !phoneNumber) {
-    return res.status(429).send({ message: "Please send phoneNumber or email or both." });
-  }
-
-  // If one information (email or phoneNumber) is provided
-  if ((!email && phoneNumber) || (email && !phoneNumber)) {
-    const find = await prisma.contact.findMany({
-      where: email ? { email } : { phoneNumber },
-      orderBy: {
-        createdAt: "asc",
-      },
-    });
-
-    if (find.length) {
-      const findAgain = await prisma.contact.findMany({
-        where: {
-          OR: [
-            { linkedId: find[0].linkedId ? find[0].linkedId : find[0].id },
-            { id: find[0].id },
-            { id: find[0].linkedId ? find[0].linkedId : find[0].id },
-          ],
-        },
-      });
-
-      return res.send(responseFormat(findAgain));
-    } else {
-      const created = await prisma.contact.create({
-        data: email
-          ? {
-              email,
-              linkPrecedence: "primary",
-            }
-          : {
-              phoneNumber,
-              linkPrecedence: "primary",
-            },
-      });
-
-      return res.send(responseFormat([created]));
-    }
-  }
-
-  
 });
 
 module.exports = identify;
