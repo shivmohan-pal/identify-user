12dab513744be2602eb5d4b9dc8ce0675bf1ef12
shivmohan-pal <64728396+shivmohan-pal@users.noreply.github.com>
2025-02-11 22:24:17 +0530

    added error handler if in case

---
+++ middleware/errorHandling.js
@@ -0,0 +1,9 @@
+const ErrorHandler = (err,_,res,next)=>{
+    const message = err.message || "Internal Server Error";
+    const status = err.status || 500;
+    res.send({status,message});
+   }
+   
+   module.exports = {
+       CreateError,ErrorHandler
+   }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Cors = require("cors");
 const identify = require("./routes/identify");
+const { ErrorHandler } = require("./middleware/errorHandling");
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use("/identify", identify);
 app.get("/", (_, res) => {
   res.send("BiteSpeed Assignment");
 });
+
+app.use(ErrorHandler);//application level error handler
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT : ${PORT}`);
